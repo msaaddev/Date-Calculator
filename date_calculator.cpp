ba28a037b9567bd6375022f5b37af71c88cfdfed
mr-saad <mohammadsaad1612@gmail.com>
2019-05-04 15:43:53 +0500

    📦 NEW: Two argument constructor

--- date_calculator.cpp
+++ date_calculator.cpp
@@ -53,10 +53,80 @@ public:
       }
   }
 
+  date2019(int dd, int mm)
+  {
+
+    if(mm > 12 || dd > 365)
+    {
+      cout << "Invalid month or date. Try Again." << endl;
+    }
+    else
+    {
+      bool check = true, check2 = false;
+      month = mm;
+      int counter = month;
+
+      while (dd > 0 && dd > 30)
+      {
+
+        cout << "Counter: " << counter << endl;
+        if(counter > 12)
+        {
+          cout << "Invalid number of days. Date can not exceed to next year i.e 2020" << endl;
+          check = false;
+          break;
+        }
+
+        if (month == 12 && dd == 31)
+        {
+          check2 = true;
+          break;
+        }
+
+        if ((counter == 1) || (counter == 3) || (counter == 5) || (counter == 7) || (counter == 8) || (counter == 10) || (counter == 12))
+        {
+            dd -= 31;
+            month++;
+        }
+        else if (counter == 2)
+        {
+          dd -= 28;
+          month++;
+        }
+        else
+        {
+          dd -= 30;
+          month++;
+        }
+
+        counter++;
+
+      }
+
+      if(check)
+      {
+        if(check2)
+        {
+          day = 31;
+          check2 = false;
+          cout << month << " " << day;
+        }
+        else
+        {
+          day = dd;
+          cout << month << " " << day;
+        }
+      }
+      // else
+      // {
+      //   cout << "Invalid number of days. Enter the days between 1 to 365." << endl;
+      // }
+    }
+  }
 
 };
 
 int main()
 {
-  date2019 d1(366);
+  date2019 d1(32, 12);
 }

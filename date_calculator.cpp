f5d147c878fa052b050dbb4e80b9d38dddca025c
mr-saad <mohammadsaad1612@gmail.com>
2019-05-04 23:04:39 +0500

    📦 NEW: setDate method

--- date_calculator.cpp
+++ date_calculator.cpp
@@ -142,13 +142,86 @@ public:
     }
   }
 
-  void setDate();
+  void setDate(int, int);
   void print();
 
 };
 
-void date2019:: setDate()
+void date2019:: setDate(int dd, int mm)
 {
+  if(mm > 12 || dd > 365)
+    {
+      cout << "Invalid month or date. Try Again." << endl;
+      day = -1;
+      month = -1;
+    }
+    else
+    {
+      bool check = true, check2 = false;
+      month = mm;
+      int counter = month;
+
+      while (dd > 0 && dd > 30)
+      {
+        if(counter > 12)
+        {
+          cout << "Invalid number of days. Date cannot exceed to next year i.e 2020" << endl;
+          check = false;
+          day = -1;
+          month = -1;
+          break;
+        }
+
+        if(month == 12 && dd > 31)
+        {
+          cout << "Invalid number of days. Date can not exceed to next year i.e 2020" << endl;
+          check = false;
+          day = -1;
+          month = -1;
+          break;
+        }
+
+        if (month == 12 && dd == 31)
+        {
+          check2 = true;
+          break;
+        }
+
+        if ((counter == 1) || (counter == 3) || (counter == 5) || (counter == 7) || (counter == 8) || (counter == 10) || (counter == 12))
+        {
+            dd -= 31;
+            month++;
+        }
+        else if (counter == 2)
+        {
+          dd -= 28;
+          month++;
+        }
+        else
+        {
+          dd -= 30;
+          month++;
+        }
+
+        counter++;
+
+      }
+
+      if(check)
+      {
+        if(check2)
+        {
+          day = 31;
+          check2 = false;
+          cout << month << " " << day;
+        }
+        else
+        {
+          day = dd;
+          cout << month << " " << day;
+        }
+      }
+    }
 }
 
 void date2019:: print()
@@ -412,6 +485,7 @@ void date2019:: print()
 
 int main()
 {
-  date2019 d1(32,12);
+  date2019 d1;
+  d1.setDate(25, 4);
   d1.print();
 }

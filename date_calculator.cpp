4292f1de5906e420e5739884b0d8a463a12b6bec
mr-saad <mohammadsaad1612@gmail.com>
2019-05-07 23:08:19 +0500

    🐛 FIX: plusDate method

--- date_calculator.cpp
+++ date_calculator.cpp
@@ -92,7 +92,7 @@ date2019::date2019(int dd, int mm)
     month = mm;
     int counter = month;
 
-    while (dd > 0 && dd > 30)
+    while (dd > 30)
     {
       if (counter > 12)
       {
@@ -103,7 +103,7 @@ date2019::date2019(int dd, int mm)
         break;
       }
 
-      if (month == 12 && dd > 31)
+      if (month == 12 && dd > 30)
       {
         cout << "Invalid number of days. Date can not exceed to next year i.e 2020" << endl;
         check = false;
@@ -177,7 +177,7 @@ void date2019::setDate(int dd, int mm)
   var = dd;
   if (mm > 12 || dd > 365)
   {
-    cout << "Invalid month or date. Try Again." << endl;
+    cout << "Invalid month or days. Try Again." << endl;
     day = -1;
     month = -1;
   }
@@ -187,7 +187,7 @@ void date2019::setDate(int dd, int mm)
     month = mm;
     int counter = month;
 
-    while (dd > 0 && dd > 30)
+    while (dd > 30)
     {
       if (counter > 12)
       {
@@ -267,6 +267,97 @@ void date2019::setDate(int dd, int mm)
         totalDays[mon - 1] + day - 1;
 }
 
+
+void date2019::plusDay(int dd)
+{
+  if (dd > 365)
+  {
+    cout << "Invalid days. Try Again." << endl;
+    day = -1;
+    month = -1;
+  }
+  else
+  {
+
+    bool check = true, check2 = false;
+    int counter = month, sub = dd;
+
+    while ( dd > 30)
+    {
+      if (counter > 12)
+      {
+        cout << "Invalid number of days. Date cannot exceed to next year i.e 2020" << endl;
+        check = false;
+        break;
+      }
+
+      if (month == 12 && dd > 30)
+      {
+        cout << "Invalid number of days. Date can not exceed to next year i.e 2020" << endl;
+        check = false;
+        break;
+      }
+
+      if (month == 12 && dd == 31)
+      {
+        check2 = true;
+        mon = month;
+        break;
+      }
+
+      if ((counter == 1) || (counter == 3) || (counter == 5) || (counter == 7) || (counter == 8) || (counter == 10) || (counter == 12))
+      {
+        if (dd == 31)
+        {
+          break;
+        }
+        else
+        {
+          dd -= 31;
+
+          month++;
+        }
+      }
+      else if (counter == 2)
+      {
+        dd -= 28;
+
+        month++;
+      }
+      else
+      {
+        dd -= 30;
+
+        month++;
+      }
+      counter++;
+    }
+
+    mon = month;
+
+    if (check)
+    {
+      if (check2)
+      {
+        day = 31;
+        check2 = false;
+      }
+      else
+      {
+        day += dd;
+      }
+    }
+    int x = 0, sum = 0, totalDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    while (x < mon)
+    {
+      sum = sum + totalDays[x];
+      x++;
+    }
+    var = sum - totalDays[mon - 1] + day - 1;
+  }
+}
+
 void date2019::print()
 {
   if (day != -1 && month != -1)
@@ -287,60 +378,11 @@ void date2019::print()
   }
 }
 
-void date2019::plusDay(int dd)
-{
-  day = 1;
-  month = 1;
-  int counter = 1;
-
-  if (dd < 365)
-  {
-    bool check = false;
-
-    while (dd > 30)
-    {
-      if ((counter == 1) || (counter == 3) || (counter == 5) || (counter == 7) || (counter == 8) || (counter == 10) || (counter == 12))
-      {
-        dd -= 31;
-        month++;
-      }
-      else if (counter == 2)
-      {
-        dd -= 28;
-        month++;
-      }
-      else
-      {
-        dd -= 30;
-        month++;
-      }
-      counter++;
-    }
-
-    if (dd >= 365)
-    {
-      cout << "Invalid number of days. Enter the days between 1 to 365." << endl;
-      month = -1;
-      day = -1;
-    }
-    else
-    {
-      day += dd;
-    }
-  }
-  else
-  {
-    cout << "Invalid number of days. Enter the days between 1 to 365." << endl;
-    month = -1;
-    day = -1;
-  }
-}
-
 int main()
 {
 
-  date2019 d1;
-  // d1.plusDay(16);
-  d1.setDate(16, 12);
+  date2019 d1(16,12);
+  d1.plusDay(3);
+  //d1.setDate(16, 12);
   d1.print();
 }
